09de6d02dac7c350c1520d45528d2b96dffcfbf8
liuhejun <liuhejun@zhuofansoft.com>
2022-01-13 10:46:29 +0800

    sonar1

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,14 @@
+public class HelloServlet {
+
+    public static void main(String[] args) {
+
+        //演示一个错误
+        int a = 10/0;
+
+        //演示代码冗余
+        int b = 100;
+        b = 200;
+
+    }
+
+}
